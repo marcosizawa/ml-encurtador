b19721f9f1fa68c87a03fbdd86274706820e6ef7
marcosizawa <50063168+marcosizawa@users.noreply.github.com>
2026-01-16 15:30:37 -0300

    Update go.py

--- api/go.py
+++ api/go.py
@@ -1,25 +1,10 @@
-from urllib.parse import parse_qs
-import json, datetime
-
 def handler(request):
-    qs = parse_qs(request.query_string.decode())
-    url = qs.get("url", [""])[0]
-
-    # log simples (opcional)
-    try:
-        with open("/tmp/cliques.log", "a") as f:
-            f.write(json.dumps({
-                "url": url,
-                "ts": datetime.datetime.utcnow().isoformat()
-            }) + "\n")
-    except:
-        pass
-
+    url = request.args.get("url", "")
     return {
         "statusCode": 302,
         "headers": {
             "Location": url
         }
     }
-fix vercel handler
+
 
